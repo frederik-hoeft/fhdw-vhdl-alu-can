cb556bfb364cfe75a50336523cfc73732ab5dffd
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2023-09-05 18:08:18 +0200

    simplify and use signed vectors

--- src/alu/alu.vhd
+++ src/alu/alu.vhd
@@ -55,15 +55,15 @@ architecture alu_beh of alu is
     signal can_reg : std_logic_vector(18 downto 0);
 
     -- registers for storing the input values
-    signal reg_a, reg_b : integer range -128 to 127;
+    signal reg_a, reg_b : signed(7 downto 0);
 
     -- signal for expanding the 8 bit input values to 16 bit
-    signal a_exp, b_exp : integer range -32768 to 32767;
+    signal a_exp, b_exp : signed(15 downto 0);
 
     -- signal for storing the output values (signed 16 bit)
-    signal result : integer range -32768 to 32767;
+    signal result : signed(15 downto 0);
 
-    signal busy : std_logic;
+    signal busy : std_logic := '0';
 begin
     -- instantiate the RAM
     ram : RAMB4_S8 port map(
@@ -81,56 +81,56 @@ begin
     begin
         if rising_edge(clk) and busy = '0' then
             reg_cmd <= cmd;
-            reg_a <= to_integer(signed(a));
-            reg_b <= to_integer(signed(b));
+            reg_a <= signed(a);
+            reg_b <= signed(b);
         end if;
     end process;
 
-    -- VHDL supports automatic sign expansion? :0
-    -- or this is just broken. either way, guess we'll figure it out :P
-    a_exp <= reg_a;
-    b_exp <= reg_b;
+    a_exp <= resize(reg_a, 16);
+    b_exp <= resize(reg_b, 16);
+    
+    ram_di <= std_logic_vector(reg_a);
+    ram_addr <= std_logic_vector("0" & reg_b);
 
     -- process for calculating the result
     calc_result : process(reg_cmd, a_exp, b_exp, reg_a, reg_b) is
     begin
+        ram_we <= '0';
         case reg_cmd is
             when "0000" => -- flow = a + b
                 result <= a_exp + b_exp;
             when "0001" => -- flow = a - b
                 result <= a_exp - b_exp;
             when "0010" => -- flow = (a + b) * 2
-                result <= to_integer(to_signed(a_exp + b_exp, 16) sll 1); -- assuming big endian
+                result <= (a_exp + b_exp) sll 1; -- assuming big endian
             when "0011" => -- flow = (a + b) * 4
-                result <= to_integer(to_signed(a_exp + b_exp, 16) sll 2); -- assuming big endian
+                result <= (a_exp + b_exp) sll 2; -- assuming big endian
             when "0100" => -- flow = -a
                 result <= -a_exp;
             when "0101" => -- flow = a << 1
-                result <= to_integer(to_signed(a_exp, 16) sll 1);
+                result <= a_exp sll 1;
             when "0110" => -- flow = a >> 1
-                result <= to_integer(to_signed(a_exp, 16) srl 1);
+                result <= a_exp srl 1;
             when "0111" => -- flow = a <<< 1 (rotate left)
-                result <= to_integer(to_signed(reg_a, 8) rol 1);
+                result <= resize(reg_a rol 1, 16);
             when "1000" => -- flow = a >>> 1 (rotate right)
-                result <= to_integer(to_signed(reg_a, 8) ror 1);
+                result <= resize(reg_a ror 1, 16);
             when "1001" => -- flow = a * b
-                result <= a_exp * b_exp;
+                result <= reg_a * reg_b;
             when "1010" => -- flow = ~(a & b) (bitwise nand)
-                result <= to_integer(to_unsigned(a_exp, 16) nand to_unsigned(b_exp, 16));
+                result <= a_exp nand b_exp;
             when "1011" => -- flow = a ^ b (bitwise xor)
-                result <= to_integer(to_unsigned(a_exp, 16) xor to_unsigned(b_exp, 16));
+                result <= a_exp xor b_exp;
             when "1100" => -- RAM [b] = a
-                ram_addr <= std_logic_vector(to_unsigned(reg_b, 9));
                 ram_we <= '1';
-                ram_di <= std_logic_vector(to_unsigned(reg_a, 8));
-                result <= reg_a;
+                result <= resize(reg_a, 16);
             when "1101" => -- flow = CRC RAM [a..b]
-                result <= 0; -- TODO
+                result <= (others => '0'); -- TODO
             when "1110" => -- can = can_reg concat RAM [a..b] (serial)
-                result <= 0; -- TODO
+                result <= (others => '0'); -- TODO
             when others => -- RESERVED
                 report "RESERVED" severity error;
-                result <= 0;
+                result <= (others => '0');
         end case;
     end process;
 
@@ -147,7 +147,7 @@ begin
     set_cout : process(reg_cmd, result, reg_a, reg_b) is
     begin
         -- set carry bit for signed 8 bit addition/subtraction
-        if (reg_cmd = "0000" and result > 127) or (reg_cmd = "0001" and to_unsigned(reg_a, 8) < to_unsigned(reg_b, 8)) then
+        if (reg_cmd = "0000" and result > 127) or (reg_cmd = "0001" and unsigned(reg_a) < unsigned(reg_b)) then
             cout <= '1';
         else
             cout <= '0';
@@ -177,8 +177,8 @@ begin
     -- process for setting the output values
     set_outputs : process(result) is
     begin
-        flow <= std_logic_vector(to_unsigned(result, 16)(7 downto 0));
-        fhigh <= std_logic_vector(to_unsigned(result, 16)(15 downto 8));
+        flow <= std_logic_vector(result(7 downto 0));
+        fhigh <= std_logic_vector(result(15 downto 8));
         cb <= '0';
         ready <= '0';
         can <= '0';
